a4a785cc9a9bcb2eb7b566900b48af5a4dc94ebd
Кащеев Илья <tactical949@gmail.com>
2020-05-07 23:25:11 +0600

    sozdal prototip funktsiyu vyvoda soobshcheniya 40/5000 created a prototype message output function

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import vk_api
-from vk_api.longpoll import VkEventType, VkLongPoll
+from vk_api.longpoll import VkEventType
+import random
+import weather
 
 
+token = '8e394514295399d4cd6d3175dac80d4268ed1065d6bc68d74d3b73b5e5ee3b98d9c7fead5fb88354b11c5'
+weather_bot = weather.Weather(token)
 
+def send_messages():
+    c_box = False  # флаги переключения
+    coord_box = False
+    for event in weather_bot.get_longpoll().listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                request = event.text.lower()
+                number = random.getrandbits(64)
+            print("Send new message:" + str(event.user_id))
+            if request == "привет":
+                weather_bot.send_main_msg(number, event)
+
+def main():
+    send_messages()
+
+if __name__ == '__main__':
+    main()
