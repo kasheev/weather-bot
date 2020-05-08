da8f6c93ee33610795cef48cc70f9ccd60adfdad
Кащеев Илья <tactical949@gmail.com>
2020-05-08 18:47:35 +0600

    created a function with weather information

--- weather.py
+++ weather.py
@@ -1,6 +1,9 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll
-from datetime import datetime
+import pyowm
+
+own = pyowm.OWM('3b4aa439966c6a305dd471efd3cabb9f', language='ru')
+
 
 class Weather:
 
@@ -8,14 +11,38 @@ class Weather:
         vk_session = vk_api.VkApi(token=token)
         self.longpoll = VkLongPoll(vk_session)
         self.vk = vk_session.get_api()
+
     def send_main_msg(self, number, event):
-       self.vk.messages.send(
+        self.vk.messages.send(
             user_id=event.user_id,
             message=(
-                    "Привет"
+                "Привет"
             ),
             random_id=number
         )
 
     def get_longpoll(self):
         return self.longpoll
+
+    def check_city(self, event, number):
+        self.vk.messages.send(
+            user_id=event.user_id,
+            message=(
+                "Введите город\n "
+            ),
+            random_id=number
+        )
+        return True
+
+    def info_weather_city(self, number, event, request):
+        observation = own.weather_at_place(str(request))
+        w = observation.get_weather()
+        temperature = w.get_temperature('celsius')['temp']
+        self.vk.messages.send(
+            user_id=event.user_id,
+            message=(
+                    'Город ' + request.title() + '\n'
+                    'Температура ' + str(int(temperature)) + '\n'
+            ),
+            random_id = number
+        )
