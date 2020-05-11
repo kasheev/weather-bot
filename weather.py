a987d13863353d61689411899227f6e4825e2d7b
Кащеев Илья <tactical949@gmail.com>
2020-05-11 23:57:06 +0600

    created an effective temperature function and a message output function and changed the function of checking the city

--- weather.py
+++ weather.py
@@ -1,8 +1,9 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll
 import pyowm
+import math
 
-own = pyowm.OWM('3b4aa439966c6a305dd471efd3cabb9f', language='ru')
+own = pyowm.OWM('3b4aa439966c6a305dd471efd3cabb9f', language= 'ru')
 
 
 class Weather:
@@ -16,7 +17,7 @@ class Weather:
         self.vk.messages.send(
             user_id=event.user_id,
             message=(
-                "Привет"
+                ""
             ),
             random_id=number
         )
@@ -24,25 +25,44 @@ class Weather:
     def get_longpoll(self):
         return self.longpoll
 
-    def check_city(self, event, number):
+    def check_city(self, request):
+        try:
+            om = own.weather_at_place(request)
+            return True
+        except Exception:
+            return False
+
+    def answer(self,event,number):
         self.vk.messages.send(
             user_id=event.user_id,
             message=(
-                "Введите город\n "
+                    'Вы ошиблись\n'
             ),
             random_id=number
         )
-        return True
+
+    def effective_temperature(self, temperature, weather):
+        wind = weather.get_wind()['speed']
+        humindity = weather.get_humidity()
+        e = (humindity / 100) * 6.105 * math.exp(17.27 * temperature / (237.7 + temperature))
+        AT = int(temperature + 0.348 * e - 0.7 * wind - 4.25) + 1
+        return AT
 
     def info_weather_city(self, number, event, request):
         observation = own.weather_at_place(str(request))
-        w = observation.get_weather()
-        temperature = w.get_temperature('celsius')['temp']
+        weather = observation.get_weather()
+        temperature = weather.get_temperature('celsius')['temp']
+        status = weather.get_detailed_status()
+        effective_temp = self.effective_temperature(temperature,weather)
+        advice = self.what_to_wear(effective_temp,status)
         self.vk.messages.send(
             user_id=event.user_id,
             message=(
                     'Город ' + request.title() + '\n'
-                    'Температура ' + str(int(temperature)) + '\n'
+                    'Температура ' + str(int(temperature)) + ' С°\n'
+                    'Ощущается как ' + str(effective_temp) + ' С°\n'
+                    'Погода ' + status.title() + ' \n'
             ),
             random_id = number
         )
+
