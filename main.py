5c640f8ea4846792caf88ae46c94de699f3d3e56
Кащеев Илья <tactical949@gmail.com>
2020-05-16 16:39:20 +0600

    added comments

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ weather_bot = weather.Weather(token)
 
 
 def send_messages():
-    c_box = False
-    for event in weather_bot.get_longpoll().listen():
-        if event.type == VkEventType.MESSAGE_NEW:
+    """отравка сообщения"""
+    c_box = False #флаг переключения
+    for event in weather_bot.get_longpoll().listen(): #цикл обработки события пользователя
+        if event.type == VkEventType.MESSAGE_NEW: #обработка нового ответа
             if event.to_me:
-                request = event.text.lower()
-                number = random.getrandbits(64)
-            print("Send new message:" + str(event.user_id))
+                request = event.text.lower() #преобразуем в текст, где все буквы в нижнем регистре
+                number = random.getrandbits(64) #случайное числло для message
+            print("Send new message:" + str(event.user_id)) #вывод информации о новом событии в консоль
 
             if request:
-                c_box = weather_bot.check_city(request)
-                if c_box:
-                    weather_bot.info_weather_city(number, event, request)
+                c_box = weather_bot.check_city(request) # с помощью флага обрабатываю сообщение(город или нет)
+                if c_box: # если True
+                    weather_bot.info_weather_city(number, event, request) # функция возвращает информацию погоды в городе
                 else:
-                    weather_bot.answer(event, number)
+                    weather_bot.answer(event, number) #функция выводит сообщение об ошибке
 
 
 def main():
     send_messages()
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #точка входа в программу
     main()
--- weather.py
+++ weather.py
@@ -10,24 +10,28 @@ own = pyowm.OWM('3b4aa439966c6a305dd471efd3cabb9f', language='ru')
 class Weather:
 
     def __init__(self, token):
+        """конструктор"""
         vk_session = vk_api.VkApi(token=token)
         self.longpoll = VkLongPoll(vk_session)
         self.vk = vk_session.get_api()
 
     def get_longpoll(self):
+      """получаю лонгпулл"""
         return self.longpoll
 
     def check_city(self, request):
+        """функиця для проверки города"""
         try:
-            om = own.weather_at_place(request)
+            om = own.weather_at_place(request)#передаю в функцию сообщение пользователя
             return True
-        except Exception:
+        except Exception: #если города нет, то выкидывает ошибку
             return False
 
     def answer(self, event, number):
-        self.vk.messages.send(
+        """выводит сообщение пользователю, если город введен неверно или произошла ошибка"""
+        self.vk.messages.send(#формирую сообщение
             user_id=event.user_id,
-            message=(
+            message=( #
                 'Что-то пошло не так...\n'
                 'Для того чтобы узнать погоду, напишите в чат интересующий вас город.\n'
                 'Например: Санкт-Петербург\n'
@@ -35,40 +39,43 @@ class Weather:
             random_id=number
         )
 
-    def effective_temperature(self, temperature, weather):
-        wind = weather.get_wind()['speed']
-        humindity = weather.get_humidity()
-        e = (humindity / 100) * 6.105 * math.exp(17.27 * temperature / (237.7 + temperature))
-        AT = int(temperature + 0.348 * e - 0.7 * wind - 4.25) + 1
-        return AT
+    def effective_temperature(self, temperature, weather):# передаю погоду и температуру
+        """считаю тепмерутуру 'по ощущениям'"""
+        wind = weather.get_wind()['speed'] #получаю скорость ветра
+        humindity = weather.get_humidity() #получаю влажность
+        e = (humindity / 100) * 6.105 * math.exp(17.27 * temperature / (237.7 + temperature)) #высчитываю коэф. e
+        AT = int(temperature + 0.348 * e - 0.7 * wind - 4.25) + 1 #высчитываю температуру 'по ощущениям'
+        return AT #возвращаю темпертару 'по ощущениям'
 
-    def what_to_wear(self, segment, status):
-        status = status.lower()
-        if segment < -3:
+    def what_to_wear(self, segment, status):# передаю сегмент и погоду
+        '''выбор того, что надеть'''
+        status = status.lower() #получаем какая сейчас погода
+        if segment < -3: #обработка критических температур по ощущениям t < -30
             return 'надеть всё или остаться дома. На улице ооочень холодно'
-        elif segment > 3:
+        elif segment > 3: # t > +30
             return 'надеть майку и шорты, ибо очень жарко'
-        try:
+        try: #проеврка на открытие файла
             with open("weather.json", "r", encoding='utf-8') as read_file:  # считываем данные из файла
                 data = json.load(read_file)
                 try:
-                    string = data['погода'][status]
-                    return string
+                    string = data['погода'][status] #обрабатываем критическую погоду:дождь, гроза
+                    return string #возвращаем что надеть
                 except KeyError:
-                    string = data['температура'][str(segment)]
-                    return string
+                    string = data['температура'][str(segment)] #обрабатываем температуру
+                    return string #возвращаем что надеть
         except FileNotFoundError:
-            return '¯\_(ツ)_/¯'
+            return '¯\_(ツ)_/¯' #если везде ошибки
 
     def info_weather_city(self, number, event, request):
-        observation = own.weather_at_place(str(request))
-        weather = observation.get_weather()
-        temperature = weather.get_temperature('celsius')['temp']
-        status = weather.get_detailed_status()
-        effective_temp = self.effective_temperature(temperature, weather)
-        segment = math.ceil(effective_temp / 10)
-        advice = self.what_to_wear(segment, status)
-        self.vk.messages.send(
+        """отправка сообщения о погоде"""
+        observation = own.weather_at_place(str(request))#получаем информацию о городе, который интересует
+        weather = observation.get_weather()#получаем погоду
+        temperature = weather.get_temperature('celsius')['temp'] #получаем температуру в цельсиях
+        status = weather.get_detailed_status() #получаем погоду
+        effective_temp = self.effective_temperature(temperature, weather)#получаем погоду по ощущениям
+        segment = math.ceil(effective_temp / 10) #делаем сегменты, для того чтобы опредлять, что надеть
+        advice = self.what_to_wear(segment, status)#получаем совет
+        self.vk.messages.send( #отправвяю сообщение
             user_id=event.user_id,
             message=(
                     'Город: ' + request.title() + '\n'
